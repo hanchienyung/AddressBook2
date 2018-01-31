c9e400b64bf384192d160b1ee762eef79dc6efb0
Addis <addwon@gmail.com>
2018-01-31 15:06:33 -0500

    Initial commit - performs add, update, delete of contacts to the Address Book.

---
+++ src/main/java/com/cy/example/demo/AddressBookRepository.java
@@ -0,0 +1,6 @@
+package com.cy.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AddressBookRepository extends CrudRepository<AddressBook2, Long> {
+}
---
+++ src/main/java/com/cy/example/demo/HomeController.java
@@ -0,0 +1,59 @@
+package com.cy.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    AddressBookRepository addressbookRepository;
+
+    @RequestMapping("/")
+    public String listAddressBooks(Model model) {
+        model.addAttribute("addressbooks", addressbookRepository.findAll());
+        return "list";
+    }
+
+    @GetMapping("/add")
+    public String addressbookForm(Model model){
+        model.addAttribute("addressbook", new AddressBook2());
+        return "addressbookform";
+
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid @ModelAttribute("addressbook") AddressBook2 addressbook, BindingResult result)
+    {
+        if (result.hasErrors()) {
+              return "addressbookform";
+        }
+        addressbookRepository.save(addressbook);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showAddressBook(@PathVariable("id") long id, Model model){
+        model.addAttribute("addressbook", addressbookRepository.findOne(id));
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateAddressBook(@PathVariable("id") long id, Model model) {
+        model.addAttribute("addressbook", addressbookRepository.findOne(id));
+        return "addressbookform";
+
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delAddressBook(@PathVariable("id") long id){
+        addressbookRepository.delete(id);
+        return "redirect:/";
+    }
+
+}
